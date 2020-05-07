cd4c4c5b44c07b4128444ca2220fc6d4911ad9ec
Jukka Aho <ahojukka5@gmail.com>
2020-05-07 21:32:29 +0700

    Add test for bmi endpoint

--- src/app.test.js
+++ src/app.test.js
@@ -8,3 +8,27 @@ describe('hello endpoint', () => {
     expect(response.text).toBe(expected);
   });
 });
+
+describe('bmi endpoint', () => {
+  test('should return bmi given properly formatted parameters', async () => {
+    const params = { height: 180, weight: 72 };
+    const response = await request(app).get('/bmi').query(params);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        height: 180,
+        weight: 72,
+        bmi: 'Normal (healthy weight)',
+      })
+    );
+  });
+
+  test('should return errors if parameters are malformatted', async () => {
+    const params = { height: 180, weight: 'I do not want to tell' };
+    const response = await request(app).get('/bmi').query(params);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: 'malformatted parameters',
+      })
+    );
+  });
+});
