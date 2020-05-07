089369ac0ddd1547a143ba93229c73a83145482b
Jukka Aho <ahojukka5@gmail.com>
2020-05-08 02:33:21 +0700

    Refactor test for hello endpoint

--- src/app.test.ts
+++ src/app.test.ts
@@ -5,7 +5,7 @@ describe('hello endpoint', () => {
   test('should return string from hello endpoint', async () => {
     const response = await request(app).get('/hello');
     const expected = 'Hello Full Stack!';
-    expect(response.text).toBe(expected);
+    expect(response.text).toEqual(expected);
   });
 });
 
