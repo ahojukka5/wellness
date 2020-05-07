70c08935a3657d496a226ff7b3fd2667be2f2b09
Jukka Aho <ahojukka5@gmail.com>
2020-05-07 21:28:55 +0700

    Add new endpoint bmi to app.js

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+const { calculateBmi } = require('./bmiCalculator.ts');
 
 const app = express();
+module.exports = app;
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-module.exports = app;
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+  if (isNaN(height) || isNaN(weight)) {
+    res.json({ error: 'malformatted parameters' });
+  } else {
+    const bmi = calculateBmi(height, weight);
+    res.json({ height, weight, bmi });
+  }
+});
