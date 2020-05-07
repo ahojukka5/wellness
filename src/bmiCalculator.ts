1fc03f8f275ca7487ee9a72e7a44125ae68c02cb
Jukka Aho <ahojukka5@gmail.com>
2020-05-07 08:13:34 +0700

    Fix typo

--- src/bmiCalculator.ts
+++ src/bmiCalculator.ts
@@ -28,7 +28,7 @@ const calculateBmi = (height: number, mass: number): string => {
   } else if (bmi < 18.5) {
     return 'Underweight';
   } else if (bmi < 25.0) {
-    return 'Normal (healty weight)';
+    return 'Normal (healthy weight)';
   } else if (bmi < 30.0) {
     return 'Overweight';
   } else if (bmi < 35.0) {
